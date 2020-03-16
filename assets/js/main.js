4e8e13d53b307f8356ea00dab297a7106850468d
Edward Agyeman <edw.agyeman@gmail.com>
2020-03-16 14:04:33 +0100

    done

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+let person = {
+    name: "Edward Agyeman",
+    alter: 26,
+    sagNameAlter() {
+        alert(`${this.name}, ${this.alter}`)
+    }
+}
+console.log(person.name)
+console.log(person.alter)
+console.log(person.sagNameAlter())
